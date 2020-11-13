a050f37ab16dd0c142cc4286af364c1d0bc8d3ec
titrxw <1120309488@qq.com>
2020-11-13 17:34:19 +0800

    添加translator facade

---
+++ src/Translator.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Rangine lang
+ *
+ * (c) We7Team 2019 <https://www.rangine.com>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com for more details
+ */
+
+namespace W7\Facade;
+
+use W7\Contract\Translation\TranslatorInterface;
+
+/**
+ * @method static mixed trans(string $key, array $replace = [], string $locale = null)
+ * @method static string transChoice(string $key, int|array|\Countable $number, array $replace = [], string $locale = null)
+ * @method static string getLocale()
+ * @method static void setLocale(string $locale)
+ * @method static string|array|null get(string $key, array $replace = [], string $locale = null, bool $fallback = true)
+ *
+ * @see \W7\Lang\Translator\Translator
+ */
+class Translator extends FacadeAbstract {
+	protected static function getFacadeAccessor() {
+		return TranslatorInterface::class;
+	}
+}
